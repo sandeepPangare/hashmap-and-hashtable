23d031335dcf9d2fdf0265a84a1cd89cd811f090
sandeepPangare <pangaresandeep96@gmail.com>
2022-09-15 23:13:33 +0530

    Ability to find frequency

---
+++ src/com/bridegelabz/Main.java
@@ -0,0 +1,12 @@
+package com.bridegelabz;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            MyHashFunction func = new MyHashFunction("To be or not to be",4);
+            //func.deleteData("Paranoids");
+            func.showTable();
+
+        }
+}
